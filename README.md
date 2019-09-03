9962420fdff4e62b2590d2e3368e2253d2278cd4
David_Izosimov <David.Izosimov@gmail.com>
2019-09-03 08:50:53 +0200

    Adds more instructions in README. and adds the account withdrawal test to atm_rspec and validation of funds

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ I want to work in a test driven way and unit test my code
 As a user
 In order to make a withdrawal
 The ATM needs to have funds
+
+As a User               
+In order to know if my withdrawal was successful or unsuccessful           
+I want to receive a message with my withdrawal details
+
+As an ATM Operator
+In order for the right person to make a withdrawal
+I want each person to have a seperate account
+
+As an ATM operator           
+In order to ensure that an Account holder can only withdraw funds that he has balance for           
+I want to allow a withdrawal only if there are sufficient funds in the account
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 require './lib/atm.rb'
 
 describe ATM do
+    let(:account) {instance_double('Account')}
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
     it 'has 1000$ on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -10,4 +17,8 @@ describe ATM do
         expect(subject.funds).to eq 950
     end
 
+    it 'allow withdraw if the account has enough balance.' do
+        expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
+        expect(subject.withdraw(45, account)).to eq expected_output
+    end
 end
