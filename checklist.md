7e3ba612ea8bd32e439ec390ada746242747aa5a
AlexSaxena <saxenaalex@gmail.com>
2019-09-03 10:52:31 +0200

    Adds checklist. extracts the check of account balance to seperate method and extract transactions to seperate method. adds sad path test rejecting if account has insufficient funds.

--- checklist.md
+++ checklist.md
@@ -2,3 +2,6 @@ Checklist for git commit
 
 C1) A -> D
 Added user story, added method account, added it test instance double, added require date, fund checking case for atm and account.
+
+C2)
+Adds checklist. extracts the check of account balance to seperate method and extract transactions to seperate method. adds sad path test rejecting if account has insufficient funds.
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -18,6 +18,11 @@ describe ATM do
         expect(subject.funds).to eq 950
     end
 
+    it 'rejects withdraw if account has insufficient funds' do
+        expected_output = {status: false, message: 'insufficient funds', date: Date.today}
+        expect(subject.withdraw(105, account)).to eq expected_output
+    end
+
     it 'allow withdraw if the account has enough balance.' do
         expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
         expect(subject.withdraw(45, account)).to eq expected_output
