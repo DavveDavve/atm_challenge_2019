5ab8456cbd2049d0f76a344d3a378b7f355edecc
David_Izosimov <David.Izosimov@gmail.com>
2019-10-14 20:05:12 +0200

    Changes the expiry date of instance double

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm.rb'
 require 'date'
 
 describe ATM do
-    let(:account) {instance_double('Account', pin_code: '1234', exp_date: '10/19', account_status: :active)}
+    let(:account) {instance_double('Account', pin_code: '1234', exp_date: '10/23', account_status: :active)}
 
     before do
         allow(account).to receive(:balance).and_return(100)
